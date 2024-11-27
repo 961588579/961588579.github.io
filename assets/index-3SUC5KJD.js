76f29443326e1f973dc145b25ed9bdbf0bb5b77e
xuechen <xue.c@luban.com>
2024-11-27 23:59:18 +0800

    chore: vite-react-ts

---
+++ assets/index-3SUC5KJD.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Cp2WuUxC.js";const e=()=>r.jsx("div",{children:"我的"});export{e as default};
---
+++ assets/index-CfH_9_1R.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-Cp2WuUxC.js";const r=()=>s.jsx("div",{children:"消息"});export{r as default};
